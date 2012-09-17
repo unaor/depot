8358c51a6a6a8d85ed3fc2215a7b67176b9951cf
Uri Naor <u_naor@yahoo.com>
2012-09-17 15:38:38 -0400

    page 90

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'c70778971e1e3cbd9d59336444a80452d5a0f6808c44a449bcca8406d98b76305f2ace634a5c547cb89c494401dbfbd6f42da2bb89e36c57d44df7fe1dee7475'
---
+++ test/unit/product_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+  test "Product should not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+  test "product price must be positive" do
+      product = Product.new(title:
+      "My Book Title",
+      description: "yyy",
+      image_url:
+      "zzz.jpg")
+      product.price = -1
+      assert product.invalid?
+      product.price=1
+      assert product.valid?
+  end
+  
+    def new_product(image_url)
+       Product.new(title:
+      "My Book Title",
+       description: "yyy",
+       price:1,
+       image_url:image_url)
+  end
+  test "image url" do
+  assert new_product('fdf.fdf').invalid?
+  assert new_product('fdf.jpg').valid?
+  end
+  
+  test "uniqueness of title" do
+    product1 = new_product('picture.jpg')
+    product2 = new_product('lame.png')
+    assert product1.valid? ,"should be valid"
+    product1.save
+    assert !product2.save ,"product 2 should not be valid"
+  end
+end
