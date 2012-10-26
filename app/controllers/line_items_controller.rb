d0bdc74397d70f541497dae6a8a4f4b206b5c2ae
Uri <u_naor@yahoo.com>
2012-10-26 18:05:56 -0400

    forgot to fix create method line

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,7 +42,7 @@ class LineItemsController < ApplicationController
   def create
     @cart =current_cart
     product=Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product.id)
+    @line_item = @cart.line_items.build(product_id: product.id)
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
