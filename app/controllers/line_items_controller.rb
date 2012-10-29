00a566e2a7da8b41b1987ec367563ebda774ca3f
Uri <u_naor@yahoo.com>
2012-10-29 18:26:16 -0400

    page 162

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,10 +42,11 @@ class LineItemsController < ApplicationController
   def create
     @cart =current_cart
     product=Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product_id: product.id)
+    @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_url }
+        format.js   {@current_item=@line_item}
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
@@ -77,7 +78,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to store_path ,notice: 'Item was removed' }
       format.json { head :no_content }
     end
   end
